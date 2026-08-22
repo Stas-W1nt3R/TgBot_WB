3456a61110cbceffa9e3232823e26756b4a79a4a
Stanislav Kondratiev <stanislaw.comza@gmail.com>
2026-08-22 20:49:07 +0300

    wrote all models

--- TgBot_WB/settings.py
+++ TgBot_WB/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TgBot_WB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TgBot_WB',
+        'USER': 'postgres',
+        'PASSWORD': 'kondratiev',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class TgUser(models.Model):
+    telegram_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'tg_user'
+        verbose_name = 'Tg User'
+        verbose_name_plural = 'Tg Users'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    stock = models.BooleanField()
+    url = models.URLField()
+    shop_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class UserTracking(models.Model):
+    user = models.ForeignKey(TgUser, on_delete=models.CASCADE)
+    target_price = models.DecimalField(decimal_places=2, max_digits=10)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'user_tracking'
+        unique_together = ('user', 'product')
+        verbose_name = 'User Tracking'
+        verbose_name_plural = 'User Tracking'
